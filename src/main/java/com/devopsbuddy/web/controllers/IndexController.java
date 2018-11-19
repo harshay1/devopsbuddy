d5f131cab956928a92830c1fd856972f9e8eebef
Harsha Yalvarthy <harsha.hy@gmail.com>
2018-11-18 18:50:32 -0600

    Added Index Controller to start the Application

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloWorldController {
+public class IndexController {
     @RequestMapping("/")
-    public String sayHello() {
+    public String home(){
         return "index";
     }
 }
