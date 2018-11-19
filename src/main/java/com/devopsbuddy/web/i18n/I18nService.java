748fcc750d81c97bcb5979891ada64e48c7d2d6c
Harsha Yalvarthy <harsha.hy@gmail.com>
2018-11-19 11:02:15 -0600

    Added i18n capabilities to the web skeleton

---
+++ src/main/java/com/devopsbuddy/web/i18n/I18nService.java
@@ -0,0 +1,33 @@
+package com.devopsbuddy.web.i18n;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+public class I18nService {
+    @Autowired
+    private MessageSource messageSource;
+
+
+    /**
+     * Returns a message for the given messageId and the default locale in the session context
+     * @param messageId the key to the messages resource file
+     */
+    public String getMessage(String messageId) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return getMessage(messageId, locale);
+    }
+
+    /**
+     * Returns a message for the given messageId and locale
+     * @param messageId the key to the messages resource file
+     * @param locale the Locale
+     */
+    public String getMessage(String messageId, Locale locale) {
+        return messageSource.getMessage(messageId,null, locale);
+    }
+}
