508b1d034daf3df67d68fb83c8ad2428570cddda
Harsha Yalvarthy <harsha.hy@gmail.com>
2018-11-27 15:55:10 -0600

    Added Logging functionality with Logback and SLF4J

--- src/main/java/com/devopsbuddy/web/i18n/I18nService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18nService.java
@@ -1,5 +1,6 @@
 package com.devopsbuddy.web.i18n;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -11,13 +12,16 @@ import java.util.Locale;
 public class I18nService {
     @Autowired
     private MessageSource messageSource;
-
-
+    
+    /** The application logger */
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(I18nService.class);
+    
     /**
      * Returns a message for the given messageId and the default locale in the session context
      * @param messageId the key to the messages resource file
      */
     public String getMessage(String messageId) {
+        LOG.info("Returning i18n text for messageId {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId, locale);
     }
